624c0ab03f47ceeca2e53ea831b988f51e61bde6
qrvd <36740654+qrvd@users.noreply.github.com>
2018-04-10 06:57:03 +0200

    Avoid naming conflict in Rectangle

--- source/Common.hpp
+++ source/Common.hpp
@@ -51,9 +51,8 @@ Vector mult(Vector a, Vector b) {
 struct Rectangle {
   int x, y, w, h, x2, y2;
 
-  Rectangle(int x, int y, int w, int h): x(x), y(y), w(x), h(x), x2(x+w), y2(y+h) {}
+  Rectangle(int _x, int _y, int _w, int _h): x(_x), y(_y), w(_w), h(_h), x2(_x+_w), y2(_y+_h) {}
   Rectangle(Vector pos, Vector size): x(pos.x), y(pos.y), w(size.x), h(size.y), x2(pos.x+size.x), y2(pos.y+size.y) {}
-  Rectangle(int w, int h): x(0), y(0), w(w), h(h), x2(0), y2(0) { }
   Rectangle(Vector size): x(0), y(0), w(size.x), h(size.y), x2(0), y2(0) {}
 };
 
