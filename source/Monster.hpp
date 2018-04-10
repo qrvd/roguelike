9c7f08802f4ce4f5003e0b2b5d57be8b9cdc7118
qrvd <36740654+qrvd@users.noreply.github.com>
2018-04-10 06:57:56 +0200

    Monsters move to unblocked tiles

--- source/Monster.hpp
+++ source/Monster.hpp
@@ -1,7 +1,8 @@
 #ifndef _MONSTER_HPP_
 #define _MONSTER_HPP_
 
-struct Level;
+#include "Tile.hpp"
+#include "Level.hpp"
 
 struct Monster {
 	Level *level;
@@ -12,6 +13,22 @@ struct Monster {
 
 	Monster(Level *level, String name, int health = 20, Vector pos = Vector())
 		: level(level), items(), name(name), health(health), pos(pos) {}
+
+    void move(int dx, int dy) {
+        move(Vector(dx, dy));
+    }
+
+    void move(Vector delta) {
+        Vector dest = pos + delta;
+        if (level) {
+            Tile *t = level->getTile(dest);
+            if (t && !t->blocked) {
+                pos = dest;
+            }
+        } else {
+            pos = dest;
+        }
+    }
 };
 
 #endif
